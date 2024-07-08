d69df5d771c1148da2d3e8ceaa1f7531f20021df
Otavio-Ferreira <otavioferreira4343@gmail.com>
2024-07-08 19:17:28 -0300

    first commit

---
+++ teste-avaliador.php
@@ -0,0 +1,10 @@
+<?php
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+
+require 'vendor/autoload.php';
+
+
---
+++ tests/services/EncerradorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Alura\Leilao\Testes\Services;
+
+use PHPUnit\Framework\TestCase;
+
+class EncerradorTest extends TestCase
+{
+    public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados(){
+        $encerrador = new Encerrador();
+    }
+}
